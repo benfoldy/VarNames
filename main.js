b3637ab2bc64312c30dbb815b9f1a74abc81e700
benfoldy <34633895+benfoldy@users.noreply.github.com>
2018-03-19 13:36:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var familyMembers= "input"
+	var arrive2018= if("yes"),"1000",else("0")
+	var arrive2019= if("yes"),
+	var english= if("yes"), else ("no")
+	var NGO = {
+		Organization: ["Help Unlimited", "New Start","Better Tomorrow","Home Safe", "New Home"]
+	}
+	var expenses:[
+	people: [1, 2, 3, 4, 5 && >5],
+	cost: [800, 950, 1000, 1100,1200]
+	]
+}
